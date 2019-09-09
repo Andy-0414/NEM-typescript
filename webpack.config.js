0901a582100b89909603bf45c11914e50f112b4d
Andy0414 <pjh8667@naver.com>
2019-09-09 21:57:33 +0900

    UPDATE webpack

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require("path");
+const nodeExternals = require("webpack-node-externals");
+
+module.exports = {
+	entry: "./src/app.ts",
+	mode: "development",
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: "ts-loader",
+				exclude: /node_modules/
+			}
+		]
+	},
+	devtool: "source-map",
+	target: "node",
+	externals: [nodeExternals()],
+	resolve: {
+		extensions: [".tsx", ".ts", ".js"]
+	},
+	output: {
+		filename: "app.js",
+		path: path.resolve(__dirname, "dist")
+	}
+};
