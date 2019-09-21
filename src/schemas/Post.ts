3ede9b52ed105b1f49c1858e68a600016e6697a0
Andy0414 <pjh8667@naver.com>
2019-09-21 13:52:12 +0900

    UPDATE post description

--- src/schemas/Post.ts
+++ src/schemas/Post.ts
@@ -15,17 +15,51 @@ export interface IPost {
  * @description Post 스키마에 대한 메서드 ( 레코드 )
  */
 export interface IPostSchema extends IPost, Document {
+	/**
+	 * @description 이 글을 삭제합니다.
+	 * @returns {Promise<boolean>} 성공 여부를 반환합니다.
+	 */
 	removePost(): Promise<any>;
+	/**
+	 * @description 이 글의 정보를 반환합니다.
+	 * @param {IUser}data 글의 바꿀 정보
+	 * @returns {Promise<IPostSchema>} 작업이 완료된 후 그 글를 반환합니다.
+	 */
 	changeInfomation(data: IPost): Promise<IPostSchema>;
+	/**
+	 * @description 이 글의 주인인지 아닌지를 판단합니다
+	 * @param owner 비교할 주인
+	 * @returns {boolean} 주인 여부를 반환합니다
+	 */
 	ownerCheck(owner: IUserSchema): boolean;
 }
 /**
  * @description Post 모델에 대한 정적 메서드 ( 테이블 )
  */
 export interface IPostModel extends Model<IPostSchema> {
+	/**
+	 * @description title과 content 필드의 유효성을 검사합니다.
+	 * @param {any}data 체크 할 객체
+	 * @returns {boolean} 유효성 결과
+	 */
 	dataCheck(data: any): boolean;
+	/**
+	 * @description 글을 생성한 후 그 글를 반환합니다.
+	 * @param {IUser}data 생성할 글 데이터
+	 * @returns {Promise<IUserSchema>} 입력받은 데이터에 대한 글입니다.
+	 */
 	createPost(owner: IUserSchema, data: IPost): Promise<IPostSchema>;
+	/**
+	 * @description 글의 아이디을 입력받아 일치하는 글를 반환합니다.
+	 * @param {ObjectID}id 찾을 글의 _id(ObjectID)
+	 * @returns {Promise<IPostSchema>} 일치하는 글를 반환합니다.
+	 */
 	findByID(id: ObjectID): Promise<IPostSchema>;
+	/**
+	 * @description 글의 주인을 입력받아 일치하는 글를 반환합니다.
+	 * @param {IUserSchema}owner 찾을 글의 주인
+	 * @returns {Promise<IPostSchema>} 일치하는 글를 반환합니다.
+	 */
 	findByOwner(owner: IUserSchema): Promise<IPostSchema[]>;
 }
 
