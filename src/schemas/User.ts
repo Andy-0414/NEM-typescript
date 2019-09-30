a17a9e0a0d01dbc3ae14ab8db4db28710b6d0ff0
Andy0414 <pjh8667@naver.com>
2019-09-30 12:30:11 +0900

    UPDATE bug fix imgPath

--- src/schemas/User.ts
+++ src/schemas/User.ts
@@ -105,7 +105,8 @@ export interface IUserModel extends Model<IUserSchema> {
 const UserSchema: Schema = new Schema({
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
-	nickname: { type: String },
+	nickname: { type: String, default: "" },
+	imgPath: { type: String, default: "" },
 	lastLogin: { type: Date, default: Date.now },
 	createAt: { type: Date, default: Date.now },
 	salt: { type: String, default: process.env.SECRET_KEY || "SECRET" }
